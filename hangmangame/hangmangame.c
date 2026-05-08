acff230a949c1309b4de597e6ed0ca69ef9681c3
Davi Rocha <davirocha.x.2.0@gmail.com>
2026-05-08 02:26:15 +0000

    problem

--- hangmangame/hangmangame.c
+++ hangmangame/hangmangame.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <string.h>
 
-int lifes = 5
+int lifes = 5;
 char a[1];
 void verification(char* rw, char* wb){
-    printf("%s", wb)
-    if(lifes = 0){
+
+    if(lifes == 0){
         printf("GAME OVER\n");
         return;
     }
-
-    scanf("%c", &a[0]);
+    printf("tete\n");
+// it's prohibiten to compute at this line
+    scanf("%c", a);
 
     if(strcspn(rw, a) == strlen(rw)){
         lifes--;
-        printf("wrong letter, %d more lifes left\n\n")
+        printf("%s\n", wb);
+        printf("wrong letter, %d more lifes left\n", lifes);
     } else {
         wb[strcspn(rw, a)] = a[0];
-        printf("%s", wb);
-    }
+        printf("%s\n", wb);
 
-    
-    if(strcspn(wb, '_') == strlen(wb){
-        printf("CONGRATULATION, YOU WON!\n");
-        return:
+        if(strcspn(wb, "-") == strlen(wb)){
+            printf("CONGRATULATION, YOU WON!\n");
+            return;
     }
-    verification(rw,wb);
+    }
+    verification(rw, wb);
 }
 
+int main(int argc, char* argv[]){
 
+    if(argc != 2){
+        printf("usage ./a.out [file]\n");
+        return 1;
+    }
 
-
-
-
-
-
-
-
-int main(void){
-
-    FILE *archive = fopen("argv[1]", "r");
+    FILE *archive = fopen(argv[1], "r");
     if(archive == NULL){
-        printf("Error, cannot open file");
+        printf("Error, cannot open file\n");
         return 1;
     }
     
@@ -52,12 +49,14 @@ int main(void){
      
     char wordbox[(int)size];
     char rightword[(int)size];
+    
     fscanf(archive, "%s", rightword);
     
-    for(int i = 0, i < (int)size, i++){
-        wordbox[i] = '_';
+    for(int i = 0; i < (int)size; i++){
+        wordbox[i] = '-';
     }
-
+    printf("Find out the word:\n");
+    printf("%s\n", wordbox);
     verification(rightword, wordbox);
     fclose(archive);
     return 0;
