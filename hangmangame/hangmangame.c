f14466809b5905e3406fe0dd1baf076a305a37ce
davialbuquerquerocha <davirocha.x.2.0@gmail.com>
2026-07-23 13:44:21 -0300

    conclued

--- hangmangame/hangmangame.c
+++ hangmangame/hangmangame.c
@@ -7,6 +7,7 @@ char a[2];
 int t = 0;
 char letters[26];
 
+
 void rightletter(char* rw, char* wb, int p){
     for(int i = p + 1; i < strlen(wb); i++){
         if(rw[i] == a[0]){
@@ -16,19 +17,27 @@ void rightletter(char* rw, char* wb, int p){
     }
 }
 
-void verification(char* rw, char* wb){
+void verification(char* rw, char* wb, char man[]){
 
     if(lifes == 0){
-        printf("G A M E   O V E R\n");
+        man[30] = '\\';
+        printf("%s", man);
+        printf("\nG A M E   O V E R\n");
         printf("the word was [%s]\n", rw);
         return;
     }
+    /*making the fork man
+    if(lifes == 4){
+        printf("%s", man);
+    }*/
 
     scanf(" %c", &a[0]);
+    printf("\033[H\033[J");
+    fflush(stdout);
     while(getchar() != '\n');
 
     if(a[0] > 122 || a[0] < 97){
-        printf("Nem vem Rademaker, são letras mínusculas!\n");
+        printf("apenas letras mínusculas!\n");
         return;
     }
     letters[t] = a[0];
@@ -37,11 +46,19 @@ void verification(char* rw, char* wb){
     letters[t + 1] = '\0';
     if(strcspn(rw, a) == strlen(rw)){
         lifes--;
+        man[20] = lifes == 4 ? '/' : man[20];
+        man[21] = lifes == 3 ? '|' : man[21];
+        man[22] = lifes == 2 ? '\\' : man[22];
+        man[29] = lifes == 1 ? '/' : man[29];
+
+        printf("%s", man);
         printf("\nwrong letter, %d more lifes left                  ( %s)\n", lifes, letters);
+
     } else {
         int p = strcspn(rw, a); 
         wb[p] = a[0];
         rightletter(rw, wb, p);
+        printf("%s", man);
         printf("\n%s                                     ( %s)\n", wb, letters);
         
 
@@ -52,10 +69,12 @@ void verification(char* rw, char* wb){
     }
     
     t++;
-    verification(rw, wb);
+    verification(rw, wb, man);
 }
 
 int main(int argc, char* argv[]){
+
+    char man[33] = "______\n|     O\n|       \n|     "; // 20 21 22 29 30
 
     if(argc != 2){
         printf("usage ./a.out [file]\n");
@@ -84,7 +103,7 @@ int main(int argc, char* argv[]){
 
     printf("Welcome to HANGMAN GAME\n Enter only lowercase letters to figure out the word:\n");
     printf("%s\n", wordbox);
-    verification(rightword, wordbox);
+    verification(rightword, wordbox, man);
     
     fclose(archive);
     
